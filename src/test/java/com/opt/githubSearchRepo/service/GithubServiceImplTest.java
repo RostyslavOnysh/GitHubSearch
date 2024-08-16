c99f64fd589bbd437c60178285e8da6fec3e5e24
Ros <traine41@icloud.com>
2024-08-16 14:40:00 +0100

    minor updates

--- src/test/java/com/opt/githubSearchRepo/service/GithubServiceImplTest.java
+++ src/test/java/com/opt/githubSearchRepo/service/GithubServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.opt.githubSearchRepo.service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,5 +12,4 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 class GithubServiceImplTest {
     @Autowired
     private WebTestClient webTestClient;
-
 }
